ce2aeb32a37be0ab5f9772742d91cd02131579fe
Ben Eggett <beneggett@gmail.com>
2014-02-13 19:21:07 -0700

    Adds an email template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@my_site.com"
 
+  layout "email_template"
+  
   def welcome(user)
     @user = User.find(user)
     mail(to: @user.email, subject: "Hey #{ @user.first_name }, Welcome to My Awesome Site")
