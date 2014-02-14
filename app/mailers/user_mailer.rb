5cf3eeb22eca15c55df2f09468d55beb1499b7fa
Ben Eggett <beneggett@gmail.com>
2014-02-13 19:08:03 -0700

    Adds welcome email & sends after user creation

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: "noreply@my_site.com"
+
+  def welcome(user)
+    @user = User.find(user)
+    mail(to: @user.email, subject: "Hey #{ @user.first_name }, Welcome to My Awesome Site")
+  end
+
+end
