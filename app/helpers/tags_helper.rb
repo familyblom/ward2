a423693b1e38fe99dee14da02a6bbf636726ba4f
Ben Eggett <beneggett@gmail.com>
2014-03-03 17:27:36 -0700

    Partway through the project portfolio changes

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -2,9 +2,9 @@ module TagsHelper
 
   def display_tags(object_with_tags)
     if object_with_tags.tags.any?
-      content_tag :span do
+      content_tag :ul, class: 'list-unstyled inline-block' do
         object_with_tags.tags.each do |tag|
-          concat content_tag(:span, tag.name, class: "label label-success margin-horizontal-5")
+          concat content_tag(:li, tag.name, class: "label label-success margin-horizontal-5")
         end
       end
     end
@@ -12,9 +12,9 @@ module TagsHelper
 
   def display_tools(project)
     if project.tools.any?
-      content_tag :span do
+      content_tag :ul, class: 'list-unstyled inline-block' do
         project.tools.each do |tool|
-          concat content_tag(:span, tool.name, class: "label label-info margin-horizontal-5")
+          concat content_tag(:li, tool.name, class: "label label-info margin-horizontal-5")
         end
       end
     end
